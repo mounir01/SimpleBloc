53f04d44bb7bae4faa70e1ced792162ec32b9767
mounir01 <mounir01101@live.fr>
2016-05-22 15:24:22 -0400

    basics in form and model view

--- SimpleBlog/SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/SimpleBlog/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,5 +13,18 @@ namespace SimpleBlog.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+            if(form.Username != "momo")
+            {
+                ModelState.AddModelError("Username", "Invalid username! ");
+                return View(form);
+            }
+            return Content("Hi there "+ form.Username+"! the form is valid");
+        }    
     }
 }
